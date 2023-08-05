ac6f52099645723e6eb3e0f095995de7a1f27def
Rene Wenz <wenz@spengergasse.at>
2023-08-05 11:49:17 +0200

    Initial commit

---
+++ apps/insta/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'front-end-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'insta';
+}
